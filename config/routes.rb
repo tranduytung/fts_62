3e1619fa18e69a96abadd0053540bd5484c519f8
tranduytung <tranduytung1994@gmail.com>
2016-07-13 15:59:32 +0700

    [Admin] sign_in, sign_out, edit profile

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,13 @@ Rails.application.routes.draw do
   get "contact" => "static_pages#contact"
 
   root "static_pages#home"
+
+  devise_for :admins, controllers: {
+    sessions: "admins/sessions"
+  }, skip: [ :passwords, :registrations]
+  devise_scope :admin do
+    get "admins/edit" => "admins/registrations#edit",
+      as: "edit_admin_registration"
+    put "admins" => "admins/registrations#update", as: "admin_registration"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713043221) do
+ActiveRecord::Schema.define(version: 20160713090513) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "name"
     t.string   "chatword_id"
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
 
   create_table "answers", force: :cascade do |t|
     t.string   "content"
