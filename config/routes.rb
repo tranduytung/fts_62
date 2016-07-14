44abf6f1c834192a76b8829bdc04bca0e124e13d
nguyenngocha <dodaihoc.abvk@gmail.com>
2016-07-14 14:31:29 +0700

    [admin] edit subject

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
   end
 
   namespace :admins do
-    resources :subjects, only: [:create, :new]
+    resources :subjects, except: [:destroy, :show, :index]
   end
 end
